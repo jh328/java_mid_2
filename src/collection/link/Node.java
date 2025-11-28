93da434485cdc3144a0efd26adce10e62908b8d5
jh328 <wnsgks32810@naver.com>
2025-11-28 12:10:49 +0900

    section 05-2,3

--- src/collection/link/Node.java
+++ src/collection/link/Node.java
@@ -7,4 +7,33 @@ public class Node {
     public Node(Object item) {
         this.item = item;
     }
+
+    /**
+     * 반복문에서 문자열을 더 할 때 빌더를 쓰는게 좋다.
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Node x = this;
+        sb.append("[");
+        while (x != null) {
+            sb.append(x.item);
+            if (x.next != null) {
+                sb.append("->");
+            }
+            x = x.next;
+        }
+        sb.append("]");
+
+//      String string = sb.toString(); // ctrl + T inline varaible
+        return sb.toString();
+    }
+
+    /*@Override
+    public String toString() {
+        return "Node{" +
+                "item=" + item +
+                ", next=" + next +
+                '}';
+    }*/
 }
